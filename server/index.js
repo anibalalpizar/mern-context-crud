b23444ffa50ebf58b6c582310f3e106cd8819ab1
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-05-07 19:47:21 -0600

    feat: Add environment variable for server port

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import postsRoutes from './routes/posts.routes.js'
 import { connect } from "./db.js";
+import { PORT } from './config.js';
 
 const app = express();
 connect();
 
 app.use(postsRoutes);
 
-app.listen(3000)
-console.log('Server on port', 3000)
+app.listen(PORT, () => console.log(`Server on port ${PORT}`));
