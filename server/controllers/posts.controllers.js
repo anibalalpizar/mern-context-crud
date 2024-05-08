a4d0ccffe2c3fc09d201b8508b74ed25183b3df5
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-05-07 20:47:09 -0600

    refactor: Update posts controller to use Post model for CRUD operations

--- server/controllers/posts.controllers.js
+++ server/controllers/posts.controllers.js
@@ -1,10 +1,23 @@
+import Post from '../models/Post.js';
 
-export const getPosts = async (req, res) => ([]);
+export const getPosts = async (req, res) => res.send(await Post.find())
 
-export const createPost = async (req, res) => res.send('new post created');
+export const createPost = async (req, res) => {
+    const { title, description } = req.body
+    const postCreated = new Post({ title, description })
+    res.json(await postCreated.save())
+}
 
-export const updatePost = async (req, res) => res.send('post updated');
+export const updatePost = async (req, res) => res.json(await Post.findByIdAndUpdate(req.params.id, req.body, { new: true }))
 
-export const deletePost = async (req, res) => res.send('post deleted');
+export const deletePost = async (req, res) => {
+    const postRemoved = await Post.findByIdAndDelete(req.params.id)
+    if (!postRemoved) return res.sendStatus(404)
+    return res.sendStatus(204)
+}
 
-export const getPostById = async (req, res) => res.send('get post by id');
+export const getPostById = async (req, res) => {
+    const post = await Post.findById(req.params.id)
+    if (!post) return res.sendStatus(404)
+    return res.json(post)
+}
