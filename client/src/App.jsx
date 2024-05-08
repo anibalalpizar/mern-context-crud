f1541bc5a8b3a072c3cb6e7eef673dc6514b4b83
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-05-08 00:03:00 -0600

    Add: react setup

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+import { HomePage, NotFoundPage, PostFormPage } from "./pages";
+
+function App() {
+  return (
+    <div className="bg-neutral-900 min-h-screen flex items-center">
+      <div className="px-10 container m-auto">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/new" element={<PostFormPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
+export default App;
