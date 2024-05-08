8c717aa413871317df16ed7cb8ff9d30c5ef1b6f
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-05-07 23:18:33 -0600

    feat: Enable file uploading functionality in app.js

--- server/app.js
+++ server/app.js
@@ -1,7 +1,12 @@
 import express from 'express';
+import fileUpload from 'express-fileupload';
 import postsRoutes from './routes/posts.routes.js';
 
 const app = express();
+app.use(fileUpload({
+    useTempFiles: true,
+    tempFileDir: './upload'
+}));
 
 app.use(express.json());
 app.use(postsRoutes);
