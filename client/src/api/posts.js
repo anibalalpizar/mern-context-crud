f59e298fccf1b0b9126f11a35070e0c53dbff3fe
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-05-10 23:59:05 -0600

    refactor: Add deletePostRequest to API module

--- client/src/api/posts.js
+++ client/src/api/posts.js
@@ -3,3 +3,5 @@ import axios from 'axios';
 export const getPostsRequest = async _ => await axios.get(`http://localhost:4000/posts`)
 
 export const createPostRequest = async post => await axios.post(`http://localhost:4000/posts`, post)
+
+export const deletePostRequest = async id => await axios.delete(`http://localhost:4000/posts/${id}`)
