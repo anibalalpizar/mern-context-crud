ca3e5d89b70c8415b3e2bab40b9938a1b8d4551f
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-05-10 23:59:34 -0600

    refactor: Add delete functionality to PostCard component

--- client/src/components/PostCard.jsx
+++ client/src/components/PostCard.jsx
@@ -1,4 +1,41 @@
+import toast from "react-hot-toast";
+import { usePostContext } from "../context/postContext";
+
 export function PostCard({ post }) {
+  const { deletePost } = usePostContext();
+  const handleDelete = (id) => {
+    toast(
+      (t) => (
+        <div>
+          <p className="text-white">
+            Are you sure you want to delete this post? <strong>{id}</strong>
+          </p>
+          <div>
+            <button
+              className="bg-red-500 hover:bg-red-400 px-3 py-2 text-sm text-white rounded-sm mx-2"
+              onClick={() => {
+                deletePost(id);
+                toast.dismiss(t.id);
+              }}
+            >
+              Yes
+            </button>
+            <button
+              className="bg-slate-400 hover:bg-slate-500 px-3 py-2 text-white rounded-sm mx-2"
+              onClick={() => toast.dismiss(t.id)}
+            >
+              No
+            </button>
+          </div>
+        </div>
+      ),
+      {
+        style: {
+          background: "#202020",
+        },
+      }
+    );
+  };
   return (
     <div
       className="bg-zinc-800 text-white rounded-smd shadow-md shadow-black
@@ -6,7 +43,15 @@ export function PostCard({ post }) {
     "
     >
       <div className="px-4 py-7">
-        <h3>{post.title}</h3>
+        <div className="flex justify-between">
+          <h3>{post.title}</h3>
+          <button
+            className="bg-red-600 text-sm px-2 py-1 rounded-sm"
+            onClick={() => handleDelete(post._id)}
+          >
+            Delete
+          </button>
+        </div>
         <p>{post.description}</p>
       </div>
     </div>
